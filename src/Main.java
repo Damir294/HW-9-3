1ecc9eabaa62d01a870864b9b27797323a016c32
Дамир <damir.obuhov@mail.ru>
2022-11-02 21:14:58 +0400

    Задача 3

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int capacity = 102;
+        int seatPlace = 60;
+        int standingPlace = 41;
+        Else{
+            System.out.println("В вагоне " + seatPlace + " сидячих мест");
+            System.out.println("В вагоне " + standingPlace + " стоячих мест");
+        }
+    }
+}
